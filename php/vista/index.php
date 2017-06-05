5b062969705a5cccda0d60a8faac0c768301fc22
JasminPaolino <jasminpaolino@gmail.com>
2017-06-05 17:00:05 -0300

    un poco de home

--- php/vista/index.php
+++ php/vista/index.php
@@ -26,6 +26,7 @@
 			</form>
 		<?php endif ; ?>
 	</header>
+	
 	<nav>
 		<ul class="topnav" id="myTopnav">
 			<li>Inicio</li>
@@ -36,8 +37,48 @@
 			<a class="icon" onclick="menu()">&#9776;</a>
 		</ul>
 	</nav>
-	<footer>
-		
+	
+	<div class="carrusel">
+		<div class="slideshow-container">
+  			<div class="mySlides fade">
+    			<div class="numbertext">1 / 3</div>
+    			<img class="img-carrusel" src="img/carrusel1.jpg" style="width:100%;">
+  			</div>
+
+  			<div class="mySlides fade">
+    			<div class="numbertext">2 / 3</div>
+    			<img class="img-carrusel" src="img/carrusel2.jpg" style="width:100%">
+  			</div>
+
+	  		<div class="mySlides fade">
+    			<div class="numbertext">3 / 3</div>
+    			<img class="img-carrusel" src="img/carrusel3.jpg" style="width:100%">
+  			</div>
+
+  			<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  			<a class="next" onclick="plusSlides(1)">&#10095;</a>
+		</div>
+		<br>
+
+		<div style="text-align:center">
+  			<span class="dot" onclick="currentSlide(1)"></span> 
+  			<span class="dot" onclick="currentSlide(2)"></span> 
+  			<span class="dot" onclick="currentSlide(3)"></span> 
+		</div>
+	</div>
+
+	<section class="descripcion">
+		<p>
+			<cite>
+				Un perro no busca autos grandes, casas lujosas o ropa de diseñadores. Con agua y comida estará bien. 
+				No les importa si eres pobre o rico. Listo o tonto. Inteligente o estúpido. Dale tu corazón y el te dará el suyo.
+				¿Cuántas personas pueden hacerte sentir así, puro y especial? ¿Cuántas personas pueden hacerte sentir extraordinario?
+			</cite>
+		</p>	
+	</section>
+	
+	<footer>	
 	</footer>
+	<script type="text/javascript" src="js/carrusel.js"></script>
 </body>
 </html>
