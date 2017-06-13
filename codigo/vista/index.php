3b26495aaa04c07a508cd46ab68be12121fafc4d
JasminPaolino <jasminpaolino@gmail.com>
2017-06-13 18:55:23 -0300

    chau comentarios

--- codigo/vista/index.php
+++ codigo/vista/index.php
@@ -4,27 +4,7 @@ include_once('../extras/Config.php');
 require_once('TemplateManager.php');
 
 Config::autoload();
-/*
-class index {
 
-	private $smarty;
-	
-	function __construct() {
-		$this->smarty = new Smarty();
-		$this->smarty->addTemplateDir(Config::getTemplatesDir());
-		$this->smarty->setCompileDir(Config::getCompileDir());
-		$this->smarty->assign('pageTitle', 'Inicio');
-	}
-
-	function display($templateFile) {
-		$this->smarty->display($templateFile);
-	}
-
-	function assign($name, $value) {
-		$this->smarty->assign($name, $value);
-	}
-}
-*/
 $tpl = new TemplateManager();
 $haySesion = SesionControlador::haySesion();
 $tpl->assign('haySesion', $haySesion);
