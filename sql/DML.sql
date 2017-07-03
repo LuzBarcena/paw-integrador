eb9e909cfadd1c0f9a812412e60310e9ad4bad00
JasminPaolino <jasminpaolino@gmail.com>
2017-07-03 19:41:52 -0300

    arreglo en imagenes referencia

--- sql/DML.sql
+++ sql/DML.sql
@@ -52,8 +52,8 @@ INSERT INTO raza(nombre) VALUES ('Terrier');
 INSERT INTO raza(nombre) VALUES ('Yorkshire Terrier');
 INSERT INTO raza(nombre) VALUES ('Otro');
 
-INSERT INTO referencia(nombre, imagen) VALUES ('Se lleva con ninios', 'ref1.jpg');
-INSERT INTO referencia(nombre, imagen) VALUES ('Se lleva con perros', 'ref2.jpg');
+INSERT INTO referencia(nombre, imagen) VALUES ('Se lleva con ninios', 'ref2.jpg');
+INSERT INTO referencia(nombre, imagen) VALUES ('Se lleva con perros', 'ref1.jpg');
 INSERT INTO referencia(nombre, imagen) VALUES ('Cuidados especiales', 'ref3.jpg');
 INSERT INTO referencia(nombre, imagen) VALUES ('Discapacitado', 'ref4.jpg');
 INSERT INTO referencia(nombre, imagen) VALUES ('Caracter especial', 'ref5.jpg');
