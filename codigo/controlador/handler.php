d3399dceda9b51b899f84af98e2a0280f390462d
LuzBarcena <mluzbarcena@gmail.com>
2017-07-13 15:19:40 -0300

    redirects a index

--- codigo/controlador/handler.php
+++ codigo/controlador/handler.php
@@ -1,27 +1,5 @@
 <?php
 include '../controlador/SesionControlador.php';
-/*
-//PARA LA SESION
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if(isset($_POST['iniciarSesion'])) {
-		 header("location:../vista/login.php");
-	}
-	if(isset($_POST['cerrarSesion'])) {
-		//voy al session controlador para destruirla
-		if(SesionControlador::destruirSesion()) {
-			header("location:../vista/index.php");
-		}
-	}
-	if(isset($_POST['registrarse'])) {
-		if(SesionControlador::destruirSesion()) {
-			header("location:../vista/registrarse.php");
-		}
-	}	
-	if(isset($_POST['perfil'])) {
-		header("location:../vista/miPerfil.php");
-	}		
-}
-*/
 //PARA LA SESION
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 	$opcion = $_GET['value'];
